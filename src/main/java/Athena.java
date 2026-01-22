bc113698ece80f461582be39c2f6927d84680aa6
Sureshkumar Sheasu <sheasusheasu@gmail.com>
2026-01-22 23:40:08 +0800

    Athena echos user

--- src/main/java/Athena.java
+++ src/main/java/Athena.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Athena {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
-                " Hello! I'm Athena\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
+        String intro = "____________________________________________________________\n" +
+                "Hello! I'm Athena\n" +
+                "What can I do for you?\n" +
                 "____________________________________________________________\n";
-        System.out.println(logo);
+
+        String outro = "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+
+        System.out.println(intro);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            String output = "____________________________________________________________\n" +
+                    input + "\n" +
+                    "____________________________________________________________\n";
+
+            System.out.println(output);
+        }
+
+        System.out.println(outro);
     }
 }
