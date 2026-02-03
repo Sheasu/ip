1249a0cb048815acc888fb32940870465f7f46eb
Sureshkumar Sheasu <sheasusheasu@gmail.com>
2026-02-03 21:21:34 +0800

    Add JUnit tests for Parser and TaskList

---
+++ src/test/java/athena/ParserTest.java
@@ -0,0 +1,27 @@
+package athena;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParserTest {
+
+    @Test
+    public void parseTodoDescription_extraSpaces_trimmedSuccess() throws AthenaException {
+        assertEquals("read book", Parser.parseTodoDescription("todo   read book  "));
+    }
+
+    @Test
+    public void parseDeadline_missingByKeyword_exceptionThrown() {
+        assertThrows(AthenaException.class, () -> {
+            Parser.parseDeadline("deadline return book /at tonight");
+        });
+    }
+
+    @Test
+    public void parseEvent_outOfOrder_exceptionThrown() {
+        assertThrows(AthenaException.class, () -> {
+            Parser.parseEvent("event party /to 10pm");
+        });
+    }
+}
---
+++ src/test/java/athena/TaskListTest.java
@@ -0,0 +1,30 @@
+package athena;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TaskListTest {
+
+    @Test
+    public void getTask_invalidIndex_exceptionThrown() {
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("test"));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            tasks.getTask(5);
+        });
+    }
+
+    @Test
+    public void getSize_afterOperations_correctCount() {
+        TaskList tasks = new TaskList();
+        assertEquals(0, tasks.getSize());
+
+        tasks.add(new Todo("1"));
+        tasks.add(new Todo("2"));
+        tasks.delete(0);
+
+        assertEquals(1, tasks.getSize());
+    }
+}
